eece443808a1319f87ab9a5013c32782f2cdae7d
Vinodhini148 <35727805+Vinodhini148@users.noreply.github.com>
2018-02-01 15:54:51 +0530

    Create natural.c

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,m;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        m+=i;
+    }
+printf("sum of given numbers %d",m);
+}
