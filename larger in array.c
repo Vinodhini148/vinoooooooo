3f34da914c5be5a47ff859751413e21ba1d633bd
Vinodhini148 <35727805+Vinodhini148@users.noreply.github.com>
2018-03-01 16:12:19 +0530

    Create larger in array.c

---
+++ larger in array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+  int a[3],i,max=0;
+  printf("enter the array");
+  for(i=1;i<=3;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+  for(i=1;i<=3;i++)
+  {
+      if(a[i>max])
+      {
+          max=a[i];
+      }
+  }
+  printf("%d",max);
+} 
